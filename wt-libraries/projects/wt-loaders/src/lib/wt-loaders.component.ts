9cf637bdf24496b382ed23a1e158090a475f8bfc
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2024-12-17 17:02:14 +0000

    wt-loaders - added wt loaders

---
+++ wt-libraries/projects/wt-loaders/src/lib/wt-loaders.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-wt-loaders',
+  imports: [],
+  template: `
+    <p>
+      wt-loaders works!
+    </p>
+  `,
+  styles: ``
+})
+export class WtLoadersComponent {
+
+}
