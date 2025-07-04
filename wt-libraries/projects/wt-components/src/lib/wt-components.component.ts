cd54fb5caac470b0811817e4c31187d6eab77c45
ferraciolliw <wiliam.ferraciolli@mhr.co.uk>
2025-07-04 10:25:00 +0100

    Removed dummy components

--- wt-libraries/projects/wt-components/src/lib/wt-components.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'wt-components',
-  imports: [],
-  template: ` <p>wt-components works!</p> `,
-  styles: ``
-})
-export class WtComponentsComponent {}
